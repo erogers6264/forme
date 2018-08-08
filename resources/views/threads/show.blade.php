20fb3efd6ffcdfc1c611a4bab984b751a17b88f2
Ethan Rogers <erogers6264@gmail.com>
2018-08-08 15:22:36 -0700

    Replies have an owner

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -19,7 +19,14 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             @foreach ($thread->replies as $reply)
+                <br>
                 <div class="card">
+                    <div class="card-header">
+                        <a href="#">
+                            {{ $reply->owner->name}}
+                        </a> said {{ $reply->created_at->diffForHumans() }}...
+
+                    </div>
 
                     <div class="card-body">
                         <article>
