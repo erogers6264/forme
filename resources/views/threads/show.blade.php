cc16ccdccb3b452703b920c116d9ef37a1a237d2
Ethan Rogers <erogers6264@gmail.com>
2018-08-08 15:33:33 -0700

    Create dedicated reply partial

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -19,21 +19,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             @foreach ($thread->replies as $reply)
-                <br>
-                <div class="card">
-                    <div class="card-header">
-                        <a href="#">
-                            {{ $reply->owner->name}}
-                        </a> said {{ $reply->created_at->diffForHumans() }}...
-
-                    </div>
-
-                    <div class="card-body">
-                        <article>
-                            <div class="body">{{ $reply->body }}</div>
-                        </article>
-                    </div>
-                </div>
+                @include('threads.reply')
             @endforeach
         </div>
     </div>
