b798e240a11fea4262e2afe41db148e85b16195d
Ethan Rogers <erogers6264@gmail.com>
2018-08-10 10:03:03 -0700

    Make it so authenticated users can reply to threads

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -9,38 +9,38 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ThreadsTest extends TestCase
 {
-	use DatabaseMigrations;
+    use DatabaseMigrations;
 
-	public function setUp()
-	{
-		parent::setUp();
+    public function setUp()
+    {
+        parent::setUp();
 
-		$this->thread = factory('App\Thread')->create();
-	}
-	
+        $this->thread = factory('App\Thread')->create();
+    }
+
     /** @test */
     public function a_user_can_view_all_threads()
     {
-    	$this->get('/threads')
-    		->assertSee($this->thread->title);
+        $this->get('/threads')
+            ->assertSee($this->thread->title);
     }
 
     /** @test */
     public function a_user_can_read_a_single_thread()
-    {    	
-    	$this->get('/threads/' . $this->thread->id)
-    		->assertSee($this->thread->title);
+    {
+        $this->get($this->thread->path())
+            ->assertSee($this->thread->title);
     }
 
     /** @test */
     public function a_user_can_read_replies_that_are_associated_with_a_thread()
     {
-    	// The thread includes replies
-    	$reply = factory('App\Reply')
-    		->create(['thread_id' => $this->thread->id]);
+        // The thread includes replies
+        $reply = factory('App\Reply')
+            ->create(['thread_id' => $this->thread->id]);
 
-		$this->get('/threads/' . $this->thread->id)
-			->assertSee($reply->body);
+        $this->get($this->thread->path())
+            ->assertSee($reply->body);
 
     }
 }
