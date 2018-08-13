fd0de1f3d8953b6ae675c27ac4a0b2b783a5f867
Ethan Rogers <erogers6264@gmail.com>
2018-08-13 12:41:41 -0700

    Merge similar tests

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -14,16 +14,12 @@ class CreateThreadsTest extends TestCase
 	/** @test */
 	function guests_may_not_create_threads()
 	{
-		$this->expectException('Illuminate\Auth\AuthenticationException');
-		$thread = make('App\Thread');
-		$this->post('/threads', $thread->toArray());
-	}
+		$this->withExceptionHandling();
 
-	/** @test */
-	function guests_cannot_see_the_create_thread_page()
-	{
-		$this->withExceptionHandling()
-			->get('/threads/create')
+		$this->get('/threads/create')
+			->assertRedirect('/login');
+
+		$this->post('/threads')
 			->assertRedirect('/login');
 	}
 
