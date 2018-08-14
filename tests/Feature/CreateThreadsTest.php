60f1969f551be0eed0508cc66ad5c8421047ce64
Ethan Rogers <erogers6264@gmail.com>
2018-08-14 09:16:37 -0700

    Add more validation tests

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -42,14 +42,36 @@ class CreateThreadsTest extends TestCase
 	/** @test */
 	function a_thread_requires_a_title()
 	{
+		$this->publishThread(['title' => null])
+			->assertSessionHasErrors('title');
+	}
+
+	/** @test */
+	function a_thread_requires_a_body()
+	{
+		$this->publishThread(['body' => null])
+			->assertSessionHasErrors('body');
+	}
+
+	/** @test */
+	function a_thread_requires_a_valid_channel()
+	{
+
+		factory('App\Channel', 2)->create();
+		
+		$this->publishThread(['channel_id' => null])
+			->assertSessionHasErrors('channel_id');
+
+		$this->publishThread(['channel_id' => 999])
+			->assertSessionHasErrors('channel_id');
+	}
+
+	public function publishThread($overrides)
+	{	
 		$this->withExceptionHandling()->signIn();
 
-		$thread = make('App\Thread', ['title' => null]);
+		$thread = make('App\Thread', $overrides);
 
-
-		$this->post('/threads', $thread->toArray())
-			->assertSessionHasErrors('title');
-
-
+		return $this->post('/threads', $thread->toArray());	
 	}
 }
